2d86eb0b586baa3b2c4dc20a0b104b390a6f696d
ondrop <endryondrop@gmail.com>
2020-03-03 17:24:39 +0300

    Update

--- Lw3/task5/index.php
+++ Lw3/task5/index.php
@@ -1,9 +1,9 @@
 <?php
 	$direction = 'C:/Study/University/Front-end/Lw/Lw3/task4/data/';
-	chdir('C:/Study/University/Front-end/Lw/Lw3/task4/data/');
+	chdir($direction);
 	$email = scandir($direction, 1);
-	$direction = $email[0];
-	$handle = fopen($direction, 'r');
-	$data = file_get_contents($direction);
+	$profile = $email[0];
+	$handle = fopen($profile, 'r');
+	$data = file_get_contents($profile);
 	fclose($handle);
 	echo $data;
