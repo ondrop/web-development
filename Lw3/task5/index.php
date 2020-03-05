b2f503a972c12278702de877cbb93df102d29b3d
ondrop <endryondrop@gmail.com>
2020-03-05 18:03:32 +0300

    Update

--- Lw3/task5/index.php
+++ Lw3/task5/index.php
@@ -1,9 +1,20 @@
 <?php
+	function getGETParameter(string $name): ?string
+	{
+		return isset($_GET[$name]) ?(string) $_GET[$name] : null;
+	};
 	$direction = 'C:/Study/University/Front-end/Lw/Lw3/task4/data/';
 	chdir($direction);
-	$email = scandir($direction, 1);
-	$profile = $email[0];
-	$handle = fopen($profile, 'r');
-	$data = file_get_contents($profile);
-	fclose($handle);
-	echo $data;
+	$email = getGETParameter('email');
+	$profile = $email . '.txt';
+	if (file_exists($profile)) 
+	{
+		$handle = fopen($profile, 'r');
+		$data = file_get_contents($profile);
+		fclose($handle);
+		echo $data;
+	}
+	else
+	{
+		echo "Файл $profile не существует";
+	};	
